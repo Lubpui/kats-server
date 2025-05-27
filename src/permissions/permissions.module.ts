ef120d896b3586d5b0a7eb6673bac4b293f740a7
HamHarry <hamza_2641@hotmail.com>
2025-05-27 17:50:01 +0700

    feat: integrate Mongoose module for Role and Permission schemas in PermissionsModule

--- src/permissions/permissions.module.ts
+++ src/permissions/permissions.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common'
 import { PermissionsService } from './permissions.service'
 import { PermissionsController } from './permissions.controller'
+import { MongooseModule } from '@nestjs/mongoose'
+import { Role, RoleSchema } from './schemas/role.schema'
+import { Permission, PermissionSchema } from './schemas/permission.schema'
+
+export const PermissionsMongoose = MongooseModule.forFeature([
+  { name: Role.name, schema: RoleSchema },
+  { name: Permission.name, schema: PermissionSchema },
+])
 
 @Module({
-  imports: [],
+  imports: [PermissionsMongoose],
   controllers: [PermissionsController],
   providers: [PermissionsService],
   exports: [PermissionsService],
