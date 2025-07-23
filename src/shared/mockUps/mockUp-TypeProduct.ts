e112c3f06d0d14af520d51e852a73a907f8ac4d9
HamHarry <hamza_2641@hotmail.com>
2025-07-23 20:30:47 +0700

    feat: add mockup data for type products with delete status

---
+++ src/shared/mockUps/mockUp-TypeProduct.ts
@@ -0,0 +1,14 @@
+import { DeleteStatus } from '../enums/delete-status.enum'
+
+export const mockUpTypeProducts = [
+  {
+    name: 'KATS',
+    code: 'K',
+    delete: DeleteStatus.ISNOTDELETE,
+  },
+  {
+    name: 'GUN',
+    code: 'G',
+    delete: DeleteStatus.ISNOTDELETE,
+  },
+]
