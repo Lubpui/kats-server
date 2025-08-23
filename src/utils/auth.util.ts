e34f1eb9680029350bf540ed5b96211dec21dc87
Lubpui <yukito3644@gmail.com>
2025-08-24 04:49:12 +0700

    log test

--- src/utils/auth.util.ts
+++ src/utils/auth.util.ts
@@ -8,38 +8,43 @@ export const createUserPayload = (
   jwtService: JwtService,
   isZant = false,
 ) => {
-  const { _id, dbname } = user
-
-  // NOTE: create token
-  const payload = { userId: _id, company: dbname, isZant }
-  const accessToken = jwtService.sign(payload, {
-    issuer: process.env.JWT_ISSUER,
-    expiresIn: process.env.JWT_EXPIRES,
-    algorithm: <any>process.env.JWT_ALGORITHM,
-  })
-  throw new NotFoundException('Login failed5.0')
-  const payloadRefreshToken = {
-    userId: _id,
-    company: dbname,
-    isRefreshToken: true,
-  }
-  const refreshToken = jwtService.sign(payloadRefreshToken, {
-    issuer: process.env.JWT_ISSUER,
-    expiresIn: process.env.JWT_REFRESH_EXPIRES,
-    algorithm: <any>process.env.JWT_ALGORITHM,
-  })
-
-  return {
-    user,
-    accessToken,
-    expiresIn: Math.floor(
-      (Date.now() + ms(isZant ? '1h' : process.env.JWT_EXPIRES)) / 1000,
-    ),
-    ...(!isZant && { refreshToken: refreshToken }),
-    ...(!isZant && {
-      refreshExpiresIn: Math.floor(
-        (Date.now() + ms(process.env.JWT_REFRESH_EXPIRES)) / 1000,
+  try {
+    const { _id, dbname } = user
+  
+    // NOTE: create token
+    const payload = { userId: _id, company: dbname, isZant }
+    const accessToken = jwtService.sign(payload, {
+      issuer: process.env.JWT_ISSUER,
+      expiresIn: process.env.JWT_EXPIRES,
+      algorithm: <any>process.env.JWT_ALGORITHM,
+    })
+    
+    const payloadRefreshToken = {
+      userId: _id,
+      company: dbname,
+      isRefreshToken: true,
+    }
+    const refreshToken = jwtService.sign(payloadRefreshToken, {
+      issuer: process.env.JWT_ISSUER,
+      expiresIn: process.env.JWT_REFRESH_EXPIRES,
+      algorithm: <any>process.env.JWT_ALGORITHM,
+    })
+  
+    return {
+      user,
+      accessToken,
+      expiresIn: Math.floor(
+        (Date.now() + ms(isZant ? '1h' : process.env.JWT_EXPIRES)) / 1000,
       ),
-    }),
+      ...(!isZant && { refreshToken: refreshToken }),
+      ...(!isZant && {
+        refreshExpiresIn: Math.floor(
+          (Date.now() + ms(process.env.JWT_REFRESH_EXPIRES)) / 1000,
+        ),
+      }),
+    }
+  } catch (error) {
+    console.log(error)
+    throw error
   }
 }
