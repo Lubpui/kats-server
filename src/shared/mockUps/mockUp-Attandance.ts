e1cb14af9d111da56339e1539a4e4e52a5d02aa5
Lubpui <yukito3644@gmail.com>
2025-09-20 23:24:41 +0700

    add attandance

---
+++ src/shared/mockUps/mockUp-Attandance.ts
@@ -0,0 +1,16 @@
+export const attendanceMockUp = [
+    {
+        _id: '1',
+        currentMonth: '2024-06-01T00:00:00.000Z',
+        attendances: [
+            {
+                employeeId: 1,
+                workDates: ['2024-06-03', '2024-06-04', '2024-06-05'],
+            },
+            {
+                employeeId: 2,
+                workDates: ['2024-06-03', '2024-06-04', '2024-06-05'],
+            }
+        ]
+    }
+]
